221d4c78e28f8d2d8d9db65ee78caf69eaeb864e
zhaoshx <281611569@qq.com>
2018-10-17 15:30:23 +0800

    测试

---
+++ springcloudcommon/src/main/java/com/jk/domain/user/User.java
@@ -0,0 +1,10 @@
+package com.jk.domain.user;
+
+import java.io.Serializable;
+
+public class User  implements Serializable {
+    private static final long serialVersionUID = 9110335048267610702L;
+
+    private String ss;
+
+}
