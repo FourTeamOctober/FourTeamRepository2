65aec8f5e03cf71b86721e57b0b8edc69303f379
zhaoshx <281611569@qq.com>
2018-10-17 15:39:45 +0800

    在comon  里面 建立了一个 user 类

--- springcloudcommon/src/main/java/com/jk/domain/user/User.java
+++ springcloudcommon/src/main/java/com/jk/domain/user/User.java
@@ -5,6 +5,45 @@ import java.io.Serializable;
 public class User  implements Serializable {
     private static final long serialVersionUID = 9110335048267610702L;
 
-    private String ss;
+    private  String   ss;
+    private  String   userName;
+    private  String   userSex;
+    private  String   userDesc;
 
+    /**
+     * 赵世雄
+     * @return
+     */
+
+    public String getSs() {
+        return ss;
+    }
+
+    public void setSs(String ss) {
+        this.ss = ss;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserDesc() {
+        return userDesc;
+    }
+
+    public void setUserDesc(String userDesc) {
+        this.userDesc = userDesc;
+    }
 }
