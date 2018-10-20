c77b536eb3c71c8cbfeb2eb7eba53d75c506facb
Fc233 <21513100@qq.com>
2018-10-20 14:00:05 +0800

    渠道消费者

--- springcloudfeign/src/main/java/com/jk/SpringcloudfeignApplication.java
+++ springcloudfeign/src/main/java/com/jk/SpringcloudfeignApplication.java
@@ -2,11 +2,12 @@ package com.jk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaClient //eurake客户端
 @EnableDiscoveryClient //服务发现
 @EnableFeignClients //feign相关注解
