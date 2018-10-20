4201adceb6594e4691ecf0a4c58200044bdd21d7
orange <619110620@qq.com>
2018-10-20 13:41:54 +0800

    查询接口

--- springcloudapi/src/main/java/com/jk/service/buyer/BuyerService.java
+++ springcloudapi/src/main/java/com/jk/service/buyer/BuyerService.java
@@ -1,4 +1,34 @@
 package com.jk.service.buyer;
 
+import com.jk.domain.inquirySheet.InquirySheet;
+import com.jk.domain.productcatgory.ProductCategory;
+import com.jk.domain.range.Range;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("buyer")
 public interface BuyerService {
+    /**
+     * 询图单列表查询
+     * @return
+     */
+    @GetMapping("selectInquiryList")
+    Map<String, Object> selectInquiryList(@RequestParam(value = "inquirySheet") String inquirySheet);
+
+    /**
+     * 查询定制需求的发布范围
+     */
+    @GetMapping("range")
+    List<Range> selectRangeList();
+
+    /**
+     * 查询定制产品所需类目
+     */
+    @GetMapping("productcategory")
+    List<ProductCategory> selectProductCategory(@RequestParam(value = "productcategoryId")String productcategoryId);
 }
